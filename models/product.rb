013e4932f6f7e1044515598ada03b838ece9cdb9
Adam Hill <adam.graham.hill@gmail.com>
2016-09-16 15:13:14 -0400

    Add sample_locations class method

--- models/product.rb
+++ models/product.rb
@@ -3,23 +3,35 @@ require 'json'
 
 class Product
 
+	attr_reader :id, :title, :location, :summary, :url, :price
+
+	LOCATIONS = ['canada', 'england', 'france', 'ireland', 'mexico', 'scotland', 'taiwan', 'us']
+
   # ping the API for the product JSON
   url = 'https://fomotograph-api.udacity.com/products.json'
   DATA = HTTParty.get(url)['photos']
 
   # initialize a Product object using a data hash
   def initialize(product_data = {})
-    @id = product_data['id']
-    @title = product_data['title']
-    @location = product_data['location']
-    @summary = product_data['summary']
-    @url = product_data['url']
-    @price = product_data['price']
+  	@id = product_data['id']
+  	@title = product_data['title']
+  	@location = product_data['location']
+  	@summary = product_data['summary']
+  	@url = product_data['url']
+  	@price = product_data['price']
   end
 
   # return an array of Product objects
   def self.all
-    DATA.map { |product| new(product) }
+  	DATA.map { |product| new(product) }
+  end
+
+  def self.sample_locations
+  	@products = []
+  	LOCATIONS.each do |location|
+  		@products.push self.all.select { |product| product.location == location }.sample
+  	end
+  	return @products
   end
 
 end
